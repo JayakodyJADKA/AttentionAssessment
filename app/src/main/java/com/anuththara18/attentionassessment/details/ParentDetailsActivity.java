68c7e6f9a354e327e8c1fd1059369583965fafe8
Anuththara18 <anuththarajayakody18@gmial.com>
2022-05-02 21:45:00 +0530

    Parents Details Screen UI Modification

--- app/src/main/java/com/anuththara18/attentionassessment/details/ParentDetailsActivity.java
+++ app/src/main/java/com/anuththara18/attentionassessment/details/ParentDetailsActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -28,11 +32,12 @@ import java.util.regex.Pattern;
 
 public class ParentDetailsActivity extends AppCompatActivity {
 
-    TextView name, email, contact, next, previous, opt1, opt2, opt3, req, diag;
+    TextView name, email, contact, opt1, opt2, opt3, req, diag;
     EditText childName, parentEmail, parentContact;
     public static String child_name, parent_email, parent_contact, diagnosis;
     RadioGroup radioGroup;
     RadioButton genderradioButton;
+    ImageButton next, previous;
 
     public static int nav = 1;
 
@@ -64,8 +69,6 @@ public class ParentDetailsActivity extends AppCompatActivity {
         parentEmail  = findViewById(R.id.parentEmail);
         parentContact  = findViewById(R.id.parentContact);
 
-        next.setText(LanguageSetter.getresources().getString(R.string.next));
-        previous.setText(LanguageSetter.getresources().getString(R.string.previous));
         name.setText(LanguageSetter.getresources().getString(R.string.childName));
         email.setText(LanguageSetter.getresources().getString(R.string.parentEmail));
         contact.setText(LanguageSetter.getresources().getString(R.string.parentContact));
@@ -74,6 +77,22 @@ public class ParentDetailsActivity extends AppCompatActivity {
         opt3.setText(LanguageSetter.getresources().getString(R.string.optional));
         req.setText(LanguageSetter.getresources().getString(R.string.req));
         diag.setText(LanguageSetter.getresources().getString(R.string.diagnosis));
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Animation animZoomOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out);
+                        next.startAnimation(animZoomOut);
+                        handler.postDelayed(this, 750);
+
+                    }
+                });
+            }
+        }, 0);
 
         child_name = childName.getText().toString();
         parent_email = parentEmail.getText().toString();
@@ -93,23 +112,25 @@ public class ParentDetailsActivity extends AppCompatActivity {
                 int selectedId = radioGroup.getCheckedRadioButtonId();
                 genderradioButton = (RadioButton) findViewById(selectedId);
                if (!isEmail(parentEmail)) {
-                    Toast.makeText(ParentDetailsActivity.this, "Enter valid email", Toast.LENGTH_SHORT).show();
-                    parentEmail.setError("Enter valid email!");
+                    Toast.makeText(ParentDetailsActivity.this, LanguageSetter.getresources().getString(R.string.email), Toast.LENGTH_SHORT).show();
+                    parentEmail.setError(LanguageSetter.getresources().getString(R.string.email));
                 }
                 else if (!isPhone(parentContact)) {
-                    Toast.makeText(ParentDetailsActivity.this, "Enter Valid Number\nEg: 0771234567", Toast.LENGTH_SHORT).show();
-                    parentContact.setError("Enter Valid Number\nEg: 0771234567");
+                    Toast.makeText(ParentDetailsActivity.this, LanguageSetter.getresources().getString(R.string.contact), Toast.LENGTH_SHORT).show();
+                    parentContact.setError(LanguageSetter.getresources().getString(R.string.contact));
                 }
                 else if(selectedId == -1){
-                    Toast.makeText(ParentDetailsActivity.this,"Nothing selected", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(ParentDetailsActivity.this,LanguageSetter.getresources().getString(R.string.empty), Toast.LENGTH_SHORT).show();
                 }
                 else{
                     diagnosis = (String) genderradioButton.getText();
                     //Toast.makeText(ParentDetailsActivity.this,genderradioButton.getText(), Toast.LENGTH_SHORT).show();
                    nav = 1;
                    if (LanguageActivity.text.equals("English")) {
+                       finish();
                        startActivity(new Intent(ParentDetailsActivity.this, ConsentFormActivity.class));
                    } else {
+                       finish();
                        startActivity(new Intent(ParentDetailsActivity.this, SinhalaConsentFormActivity.class));
                    }
                 }
