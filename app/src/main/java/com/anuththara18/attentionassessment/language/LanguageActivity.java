ea360eec09578eb4f917baaebc5da173ff5ca843
Anuththara18 <anuththarajayakody18@gmial.com>
2022-05-02 17:09:54 +0530

    Language Selection Screen UI Modification

--- app/src/main/java/com/anuththara18/attentionassessment/language/LanguageActivity.java
+++ app/src/main/java/com/anuththara18/attentionassessment/language/LanguageActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AbsoluteLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,14 +24,21 @@ import com.anuththara18.attentionassessment.R;
 import com.anuththara18.attentionassessment.details.ParentDetailsActivity;
 import com.anuththara18.attentionassessment.gender.GenderActivity;
 
+import pl.droidsonroids.gif.GifImageView;
+
 public class LanguageActivity extends AppCompatActivity {
 
-    ImageButton english_btn, sinhala_btn, sinhala_txt_btn;
+    ImageView english_btn, sinhala_btn;
     TextView english_txt;
     public static String text = "";
     public static final String sharedKey = "Language";
-    TextView selectLanguage, next;
+    TextView selectLanguage;
+    ImageButton next, sinhala_txt_btn;
     public static SharedPreferences sharedPref;
+    GifImageView red_circle;
+
+    int click = 1;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +52,137 @@ public class LanguageActivity extends AppCompatActivity {
         decorView.setSystemUiVisibility(uiOptions);
         setContentView(R.layout.activity_language);
 
-        english_btn = (ImageButton)findViewById(R.id.english_btn);
-        sinhala_btn = (ImageButton)findViewById(R.id.sinhala_btn);
+        english_btn = (ImageView)findViewById(R.id.english_btn);
+        sinhala_btn = (ImageView)findViewById(R.id.sinhala_btn);
         sinhala_txt_btn = (ImageButton)findViewById(R.id.sinhala_txt_btn);
         english_txt = (TextView) findViewById(R.id.english_txt);
         selectLanguage = (TextView) findViewById(R.id.selectLanguage);
-        next = (TextView) findViewById(R.id.next);
+        next = (ImageButton) findViewById(R.id.next);
+        red_circle = findViewById(R.id.red_circle);
+
         sharedPref = getSharedPreferences(sharedKey, MODE_PRIVATE);
+
+        int bt_notes_blink = 0;
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        /*
+                        Animation animation1 =
+                                AnimationUtils.loadAnimation(getApplicationContext(),
+                                        R.anim.blink);
+                        next.startAnimation(animation1);
+                        Animation animZoomIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
+                        next.startAnimation(animZoomIn);
+
+                         */
+                        Animation animZoomOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out);
+                        next.startAnimation(animZoomOut);
+                        handler.postDelayed(this, 750);
+
+                    }
+                });
+            }
+        }, 0);
+
+        english_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp = MediaPlayer.create(getApplicationContext(), R.raw.button_click);
+                mp.start();
+                red_circle.setVisibility(View.VISIBLE);
+                sinhala_btn.setImageAlpha(127);
+                sinhala_txt_btn.setAlpha(0.5f);
+                english_btn.setImageAlpha(255);
+                english_txt.setAlpha(1f);
+                text = "English";
+                setLangPref();
+                changeLanguage();
+            }
+        });
+
+        english_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp = MediaPlayer.create(getApplicationContext(), R.raw.button_click);
+                mp.start();
+                red_circle.setVisibility(View.VISIBLE);
+                sinhala_btn.setImageAlpha(127);
+                sinhala_txt_btn.setAlpha(0.5f);
+                english_btn.setImageAlpha(255);
+                english_txt.setAlpha(1f);
+                text = "English";
+                setLangPref();
+                changeLanguage();
+            }
+        });
+
+        sinhala_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp = MediaPlayer.create(getApplicationContext(), R.raw.button_click);
+                mp.start();
+                red_circle.setVisibility(View.VISIBLE);
+                english_btn.setImageAlpha(127);
+                english_txt.setAlpha(0.5f);
+                sinhala_btn.setImageAlpha(255);
+                sinhala_txt_btn.setAlpha(1f);
+                text = "සිංහල";
+                setLangPref();
+                changeLanguage();
+            }
+        });
+
+        sinhala_txt_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp = MediaPlayer.create(getApplicationContext(), R.raw.button_click);
+                mp.start();
+                red_circle.setVisibility(View.VISIBLE);
+                english_btn.setImageAlpha(127);
+                english_txt.setAlpha(0.5f);
+                sinhala_btn.setImageAlpha(255);
+                sinhala_txt_btn.setAlpha(1f);
+                text = "සිංහල";
+                setLangPref();
+                changeLanguage();
+            }
+        });
+
+        /*
+        toggle_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                click++;
+                mp = MediaPlayer.create(getApplicationContext(), R.raw.button_click);
+                mp.start();
+                if(click % 2 == 0) {
+                    toggle_btn.setBackgroundResource(R.drawable.toggle_right);
+                    english_btn.setImageAlpha(127);
+                    english_txt.setAlpha(0.5f);
+                    sinhala_btn.setImageAlpha(255);
+                    sinhala_txt_btn.setAlpha(1f);
+                    text = "සිංහල";
+                    setLangPref();
+                    changeLanguage();
+                }
+                else {
+                    toggle_btn.setBackgroundResource(R.drawable.toggle_left);
+                    sinhala_btn.setImageAlpha(127);
+                    sinhala_txt_btn.setAlpha(0.5f);
+                    english_btn.setImageAlpha(255);
+                    english_txt.setAlpha(1f);
+                    text = "English";
+                    setLangPref();
+                    changeLanguage();
+                }
+            }
+        });
+
 
         sinhala_btn.getLayoutParams().height = 656;
         sinhala_btn.getLayoutParams().width= 656;
@@ -58,34 +195,7 @@ public class LanguageActivity extends AppCompatActivity {
         english_btn.requestLayout();
         english_txt.setPadding(0, 10, 0,0 );
         english_txt.setTextSize(40);
-
-        english_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                englishSelected();
-            }
-        });
-
-        english_txt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                englishSelected();
-            }
-        });
-
-        sinhala_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sinhalaSelected();
-            }
-        });
-
-        sinhala_txt_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                sinhalaSelected();
-            }
-        });
+         */
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,6 +204,8 @@ public class LanguageActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Please select a language", Toast.LENGTH_SHORT).show();
                 }
                 else {
+                    mp.pause();
+                    finish();
                     Intent intent = new Intent(LanguageActivity.this, GenderActivity.class);
                     startActivity(intent);
                 }
@@ -138,7 +250,6 @@ public class LanguageActivity extends AppCompatActivity {
     public void changeLanguage() {
         LanguageSetter.setLanguage(text);
         LanguageSetter.changeLanguage(text, LanguageActivity.this);
-        next.setText(LanguageSetter.getresources().getString(R.string.next));
         selectLanguage.setText(LanguageSetter.getresources().getString(R.string.language));
     }
 
